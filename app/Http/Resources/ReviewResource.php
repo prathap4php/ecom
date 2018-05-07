97a8e047595cb8aef8c4413bc02aa328ca28ba26
prathap4php <prathap4php@gmail.com>
2018-05-07 20:50:23 +0530

    Review resource and product pagination

--- app/Http/Resources/ReviewResource.php
+++ app/Http/Resources/ReviewResource.php
@@ -1,13 +1,13 @@
 <?php
 
-namespace App\Http\Resources\Review;
+namespace App\Http\Resources;
 
-use Illuminate\Http\Resources\Json\ResourceCollection;
+use Illuminate\Http\Resources\Json\JsonResource;
 
-class ReviewCollection extends ResourceCollection
+class ReviewResource extends JsonResource
 {
     /**
-     * Transform the resource collection into an array.
+     * Transform the resource into an array.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return array
